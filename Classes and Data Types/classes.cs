48227af239a6b2d896ae6aa0be4fe38d2e7af49c
jeffcarbine <jeffcarbine@gmail.com>
2015-10-07 15:51:15 -0600

    More answers!

---
+++ Classes and Data Types/classes.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using System.Collections;
+
+public class SingleCharacterScript : MonoBehaviour
+{
+    public class Stuff
+    {
+        public int bullets;
+        public int grenades;
+        public int rockets;
+
+        public Stuff(int bul, int gre, int roc)
+        {
+            bullets = bul;
+            grenades = gre;
+            rockets = roc;
+        }
+    }
+
+
+    public Stuff myStuff = new Stuff(10, 7, 25);
+    public float speed;
+    public float turnSpeed;
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+
+
+    void Update ()
+    {
+        Movement();
+        Shoot();
+    }
+
+
+    void Movement ()
+    {
+        float forwardMovement = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float turnMovement = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * forwardMovement);
+        transform.Rotate(Vector3.up * turnMovement);
+    }
+
+
+    void ShootGun ()
+    {
+        if(Input.GetButtonDown("Fire1") && myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            myStuff.bullets--;
+        }
+    }
+
+    void ShootRockets ()
+    {
+        if(Input.GetButtonDown("Fire2") && myStuff.rockets > 0)
+        {
+            // script shoots rockets
+            myStuff.rockets--;
+        }
+    }
+
+    void ThrowGrenade ()
+    {
+        if(Input.GetButtonDown("Grenade1") && myStuff.grenades > 0)
+        {
+            // script throws grenades
+            myStuff.grenades--;
+        }
+    }
+}
